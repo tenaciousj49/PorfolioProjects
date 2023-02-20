91a5c6fcb07f5e71e2a89892bc12a7546245040b
tenaciousj49 <39495846+tenaciousj49@users.noreply.github.com>
2023-02-20 10:49:33 -0600

    Update and rename ProjectPortfolio.sql to CovidProjectPortfolio.sql

--- CovidProjectPortfolio.sql
+++ CovidProjectPortfolio.sql
@@ -3,13 +3,11 @@ FROM PortfolioProject..CovidDeaths$
 Where continent is not null
 order by 3,4
 
-
-
 --SELECT*
 --FROM PortfolioProject..CovidVaccinations$
 --order by 3,4
 
---Select the Data we are going to be using
+--Select the data we are going to be using
 
 SELECT Location, Date, Total_cases, new_cases, total_deaths, population
 FROM PortfolioProject..CovidDeaths$
@@ -50,11 +48,7 @@ Where continent is not null
 Group by Location
 order by TotalDeathCount desc
 
-
-
 --let's break things down by continent
-
-
 
 -- Showing the continents with the highest death count per population
 
@@ -66,7 +60,6 @@ Group by continent
 order by TotalDeathCount desc
 
 --GLOBAL NUMBERS
-
 
 SELECT SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM
 (new_cases)*100 as DeathPercentage
@@ -100,8 +93,6 @@ From PopvsVac
 --Use CTE for RollingPeopleVaccinated since the table doesn't exist
 
 --With PopsVac as 
-
-
 
 --select*
 --from PortfolioProject..CovidDeaths$ dea
@@ -139,7 +130,6 @@ Select*, (RollingPeopleVaccinated/population)*100
 From #PercentPopluationVaccinated
 
 --Creating  A View to store data for later visualizations
-
 
 Create view PercentPopluationVaccinated as 
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
